3f8a460b6c25390dbad0fe96d83e1cfa9a3adce9
Jay Canastra <canjayj@gmail.com>
2026-05-15 16:38:12 +0000

    File changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row items-center justify-between gap-6">
           <div className="flex items-center">
             <img
-              src="https://media.base44.com/images/public/6a073c2cf9669ff20d3a8fe6/d9d3b610a_image.png"
+              src="https://media.base44.com/images/public/6a073c2cf9669ff20d3a8fe6/04a056078_image.png"
               alt="Ocean Dreamer Island Tours"
-              className="h-14 w-auto object-contain mix-blend-screen"
+              className="h-16 w-auto object-contain"
             />
           </div>
 
